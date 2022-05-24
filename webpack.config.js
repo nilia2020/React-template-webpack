8b19136686c05da73ba7ad762a1cf19477523c78
jorge niglia <jorge.niglia@gmail.com>
2022-05-24 08:20:49 -0300

    routes-ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const port = process.env.PORT || 3000;
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: "/",
   },
   mode: "development",
   resolve: {
@@ -44,10 +46,9 @@ module.exports = {
     }),
   ],
   devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    compress: true,
-    port: 3005,
+    host: "localhost",
+    port: port,
+    historyApiFallback: true,
+    open: true,
   },
 };
