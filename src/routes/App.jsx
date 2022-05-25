252b7d66a7cb01b328ecf24d2cb3b52170eba484
jorge niglia <jorge.niglia@gmail.com>
2022-05-25 13:48:06 -0300

    right

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
 import Login from "../containers/Login";
 import RecoveryPassword from "../containers/RecoveryPassword";
@@ -9,16 +9,14 @@ import NotFound from "../pages/NotFound";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="recovery-password" element={<RecoveryPassword />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="recovery-password" element={<RecoveryPassword />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
   );
 };
 
