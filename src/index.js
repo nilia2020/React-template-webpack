50b2142c2da1406899634e40f3ee32204a51567b
jorge niglia <jorge.niglia@gmail.com>
2022-05-25 13:44:03 -0300

    root

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./routes/App";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<App />);
