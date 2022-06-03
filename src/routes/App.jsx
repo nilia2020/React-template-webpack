ab6a9d52bd9b306a93f782d2c578f27bc14c0b4d
jorge niglia <jorge.niglia@gmail.com>
2022-06-03 15:04:37 -0300

    template01

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "@containers/Layout";
-import Login from "@pages/Login";
 import "@styles/global.css";
-import Home from "@pages/Home";
 import NotFound from "@pages/NotFound";
-import PasswordRecovery from "@pages/PasswordRecovery";
-import SendEmail from "@pages/SendEmail";
-import NewPassword from "@pages/NewPassword";
-import MyAccount from "@pages/MyAccount";
-import CreateAccount from "@pages/CreateAccount";
-import Checkout from "@pages/checkout";
-import Orders from "@pages/Orders";
 import AppContext from "@context/AppContext";
 import useInitialState from "@hooks/useInitialState";
 
@@ -21,15 +12,6 @@ const App = () => {
     <AppContext.Provider value={initialState}>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="password-recovery" element={<PasswordRecovery />} />
-          <Route path="/send-email" element={<SendEmail />} />
-          <Route path="/new-password" element={<NewPassword />} />
-          <Route path="/account" element={<MyAccount />} />
-          <Route path="/signup" element={<CreateAccount />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/orders" element={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
