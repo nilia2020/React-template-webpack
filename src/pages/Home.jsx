45ba52d5e015058100a15e2519e8ac3791af3e19
jorge niglia <jorge.niglia@gmail.com>
2022-06-04 12:01:26 -0300

    template2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <h1 className="text-center text-2xl mt-2 font-semibold">Template</h1>;
+  return (
+    <h1 className="text-center text-2xl mt-2 font-semibold">
+      Template React Webpack
+    </h1>
+  );
 };
 
 export default Home;
